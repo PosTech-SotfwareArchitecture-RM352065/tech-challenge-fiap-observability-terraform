035fc4f2feefc1a88ace46fead0934943e13ef30
cangelosilima <cangelosilima@gmail.com>
2024-03-16 23:50:18 -0300

    ci: destroy even if contains resources

--- azure/main.tf
+++ azure/main.tf
@@ -11,7 +11,11 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 resource "azurerm_resource_group" "observability_group" {
